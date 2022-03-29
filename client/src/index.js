6646a1b248abe9c58145cca73125f3b9c7c4135e
Dinesh Rout <rddinesh779@gmail.com>
2022-03-29 14:42:41 +0530

    feat: routes added

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AppContextProvider } from './context/AppContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import CompanyDashboard from './pages/company/CompanyDashboard';
+import ApplicantDashboard from './pages/applicant/ApplicantDashboard';
+import CompanyDetails from './pages/company/CompanyDetails';
+import CompanyLogin from './pages/company/CompanyLogin';
+import CompanyRegister from './pages/company/CompanyRegister';
+import ApplicantDetails from './pages/applicant/ApplicantDetails';
+import ApplicantLogin from './pages/applicant/ApplicantLogin';
+import ApplicantRegister from './pages/applicant/ApplicantRegister';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<Welcome />} />
+            <Route path='company' element={<CompanyDashboard />}>
+              <Route index element={<CompanyDashboard />} />
+              <Route path=':id' element={<CompanyDetails />} />
+              <Route path='login' element={<CompanyLogin />} />
+              <Route path='register' element={<CompanyRegister />} />
+            </Route>
+            <Route path='applicant' element={<ApplicantDashboard />}>
+              <Route index element={<ApplicantDashboard />} />
+              <Route path=':id' element={<ApplicantDetails />} />
+              <Route path='login' element={<ApplicantLogin />} />
+              <Route path='register' element={<ApplicantRegister />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
