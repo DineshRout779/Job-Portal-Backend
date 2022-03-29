68e20f88da0a5fa8f89f5cd2dc86bc329a5e5df0
Dinesh Rout <rddinesh779@gmail.com>
2022-03-29 14:39:43 +0530

    feat: routes added

---
+++ server/routes/applicant.js
@@ -0,0 +1,19 @@
+const { getApplicant } = require('../controllers/applicant');
+
+const router = require('express').Router();
+
+router.param('applicantId', getApplicantById);
+
+// // get all applicants
+// router.get('/', registerAsUser);
+
+// get applicant
+router.get('/:applicantId', getApplicant);
+
+// edit applicant
+// router.put('/:applicantId', updateApplicant);
+
+// // delete applicant
+// router.delete('/:applicantId', deleteApplicant);
+
+module.exports = router;
