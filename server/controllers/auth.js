3cc6be9f7eec11354dda95b3b3323d720b47fa3b
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:34:18 +0530

    feat: login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,14 @@
 const Applicant = require('../models/Applicant');
 const bcrypt = require('bcrypt');
 const Company = require('../models/Company');
+const jwt = require('jsonwebtoken');
 
 exports.registerAsCompany = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.status(400).json({
-      error: 'Please provide username, email and password!',
+      error: 'Please provide name, email and password!',
     });
   }
 
@@ -21,7 +22,7 @@ exports.registerAsCompany = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, salt);
 
   try {
-    const newCompany = new User({
+    const newCompany = new Company({
       name,
       email,
       password: hashedPassword,
@@ -37,7 +38,7 @@ exports.registerAsCompany = async (req, res) => {
   }
 };
 
-exports.registerAsApplicant = (req, res) => {
+exports.registerAsApplicant = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -56,7 +57,7 @@ exports.registerAsApplicant = (req, res) => {
   const hashedPassword = await bcrypt.hash(password, salt);
 
   try {
-    const newApplicant = new User({
+    const newApplicant = new Applicant({
       name,
       email,
       password: hashedPassword,
@@ -72,7 +73,102 @@ exports.registerAsApplicant = (req, res) => {
   }
 };
 
-exports.loginAsCompany = () => {};
-exports.loginAsApplicant = () => {
-  //
+exports.loginAsCompany = async (req, res) => {
+  const { email, password: clientPassword } = req.body;
+
+  if (!email || !clientPassword) {
+    return res.status(400).json({
+      error: 'Please provide email and password!',
+    });
+  }
+
+  try {
+    const company = await Company.findOne({ email });
+
+    if (!company) {
+      return res.status(404).json({
+        error: 'Company not registered!',
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      clientPassword,
+      company.password
+    );
+    if (!validPassword) {
+      return res.status(403).json({
+        error: 'Invalid credentials!',
+      });
+    }
+
+    const token = jwt.sign({ _id: company._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    // res.cookie('token', token, { expire: new Date() + 9999 });
+
+    const { password, ...others } = company._doc;
+
+    return res.status(200).json({
+      user: {
+        ...others,
+        token,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      err: err.message,
+    });
+  }
+};
+
+exports.loginAsApplicant = async (req, res) => {
+  const { email, password: clientPassword } = req.body;
+
+  if (!email || !clientPassword) {
+    return res.status(400).json({
+      error: 'Please provide email and password!',
+    });
+  }
+
+  try {
+    const applicant = await Applicant.findOne({ email });
+
+    if (!applicant) {
+      return res.status(404).json({
+        error: 'Company not registered!',
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      clientPassword,
+      applicant.password
+    );
+    if (!validPassword) {
+      return res.status(403).json({
+        error: 'Invalid credentials!',
+      });
+    }
+
+    const token = jwt.sign({ _id: applicant._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    // res.cookie('token', token, { expire: new Date() + 9999 });
+
+    const { password, ...others } = applicant._doc;
+
+    return res.status(200).json({
+      user: {
+        ...others,
+        token,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      err: err.message,
+    });
+  }
 };
