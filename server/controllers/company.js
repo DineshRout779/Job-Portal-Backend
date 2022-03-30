ecc5f28184ef511fa9a58be81d27fc3a0a1c0e14
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:34:29 +0530

    feat: get company

--- server/controllers/company.js
+++ server/controllers/company.js
@@ -1,7 +1,22 @@
-exports.getCompanyById = () => {
-  //
+const Company = require('../models/Company');
+
+exports.getCompanyById = async (req, res, next, id) => {
+  try {
+    let company = await Company.findById(id);
+    if (!company) {
+      return res.status(404).json({
+        error: 'Applicant not found',
+      });
+    }
+
+    req.company = company;
+    next();
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-exports.getCompany = () => {
-  //
+exports.getCompany = (req, res) => {
+  req.company.password = undefined;
+  return res.json(req.company);
 };
