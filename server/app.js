4c67eab0d87d2759649cf798354e3eb5540868cb
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:33:48 +0530

    added routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+const jobRoutes = require('./routes/job');
+const applicantRoutes = require('./routes/applicant');
+const companyRoutes = require('./routes/company');
 require('dotenv').config();
 
 const app = express();
@@ -18,9 +22,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // routes
-app.get('/api', (req, res) => {
-  res.send('Hello world!!');
-});
+app.use('/api/auth', authRoutes);
+app.use('/api/job', jobRoutes);
+app.use('/api/applicant', applicantRoutes);
+app.use('/api/company', companyRoutes);
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
