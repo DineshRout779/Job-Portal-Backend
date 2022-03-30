6a5214ec24a845b9381d4c8f149ed0f3437b4565
Dinesh Rout <rddinesh779@gmail.com>
2022-03-30 17:26:36 +0530

    added reducers

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -5,6 +5,21 @@ const reducers = (state, action) => {
         ...state,
         user: action.payload,
       };
+    case 'USER_TYPE_APPLICANT':
+      return {
+        ...state,
+        isApplicant: true,
+      };
+    case 'USER_TYPE_COMPANY':
+      return {
+        ...state,
+        isApplicant: false,
+      };
+    case 'ADD_JOB':
+      return {
+        ...state,
+        jobs: [action.payload, ...state.jobs],
+      };
     default:
       return state;
   }
