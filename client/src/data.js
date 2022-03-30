74be8e32e3307ac5c758f392e8a811ff740d46d3
Dinesh Rout <rddinesh779@gmail.com>
2022-03-30 17:25:53 +0530

    added mock data

--- client/src/data.js
+++ client/src/data.js
@@ -54,3 +54,50 @@ export const jobs = [
     location: 'remote',
   },
 ];
+
+export const applicants = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    desc: 'A Full-Stack web developer',
+    location: 'Los Angeles, CA',
+  },
+  {
+    id: 2,
+    name: 'Dinesh Rout',
+    email: 'dineshrout@gmail.com',
+    desc: 'A frontend web developer',
+    location: 'Los Angeles, CA',
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: 'Amazon',
+    email: 'amazon@gmail.com',
+    desc: 'A Well Funded Company.',
+    jobs: [
+      {
+        id: 4,
+        title: 'Full Stack Web Developer',
+        company: 'BigBinary',
+        position: 'Senior Software Enginner',
+        jobType: 'Full-time',
+        applicants: [],
+        location: 'remote',
+      },
+      {
+        id: 5,
+        title: 'Front End Web Developer',
+        company: 'Netflix',
+        position: 'Junior Software Enginner',
+        jobType: 'Full-time',
+        applicants: [],
+        location: 'remote',
+      },
+    ],
+    location: 'Los Angeles, CA',
+  },
+];
