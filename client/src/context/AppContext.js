c7f106b646e0faaab4ae68b8698fc29be85810e4
Dinesh Rout <rddinesh779@gmail.com>
2022-03-30 17:26:18 +0530

    refactoring

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,22 +1,52 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
+import { jobs } from '../data';
 import reducers from './reducers';
 
 const AppContext = createContext();
 
 const INITIAL_STATE = {
   user: {
-    id: 'iuhf78y73uuey9',
-    name: 'John Doe',
-    email: 'johndoe@gmail.com',
+    id: 1,
+    name: 'Amazon',
+    email: 'amazon@gmail.com',
     desc: 'A frontend web developer',
+    jobs: [
+      {
+        id: 4,
+        title: 'Full Stack Web Developer',
+        company: 'BigBinary',
+        position: 'Senior Software Enginner',
+        jobType: 'Full-time',
+        applicants: [],
+        location: 'remote',
+      },
+      {
+        id: 5,
+        title: 'Front End Web Developer',
+        company: 'Netflix',
+        position: 'Junior Software Enginner',
+        jobType: 'Full-time',
+        applicants: [],
+        location: 'remote',
+      },
+    ],
     location: 'Los Angeles, CA',
   },
   error: false,
-  isApplicant: true,
+  isApplicant: false,
+  jobs: [...jobs],
 };
 
 export const AppContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducers, INITIAL_STATE);
+
+  useEffect(() => {
+    if (state.user.hasOwnProperty('jobs')) {
+      dispatch({ type: 'USER_TYPE_COMPANY' });
+    } else {
+      dispatch({ type: 'USER_TYPE_APPLICANT' });
+    }
+  }, [state.user]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
