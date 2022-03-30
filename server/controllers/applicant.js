583d83119e047ebdb8c1b4148ce3197a34559364
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:34:02 +0530

    feat: get applicant

--- server/controllers/applicant.js
+++ server/controllers/applicant.js
@@ -1,9 +1,24 @@
-exports.getApplicantById = () => {
-  //
+const Applicant = require('../models/Applicant');
+
+exports.getApplicantById = async (req, res, next, id) => {
+  try {
+    let applicant = await Applicant.findById(id);
+    if (!applicant) {
+      return res.status(404).json({
+        error: 'Applicant not found',
+      });
+    }
+
+    req.applicant = applicant;
+    next();
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-exports.getApplicant = () => {
-  //
+exports.getApplicant = (req, res) => {
+  req.applicant.password = undefined;
+  return res.json(req.applicant);
 };
 
 exports.updateApplicant = () => {
